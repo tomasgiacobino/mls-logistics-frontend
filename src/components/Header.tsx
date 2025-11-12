3fede86630ef487ce9997d7d86fc0b44ac126484
tomas <tomas.giacobino@gmail.com>
2025-11-11 21:00:58 -0300

    Prueba header mobile
    prueba para ver si el header queda bien en mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Container, Nav, Navbar} from 'react-bootstrap';
 import { useNavigate, useLocation } from 'react-router-dom';
-// import logo from '../assets/images/logos/Recurso 18.png'; // 👈 Importa tu logo aquí
+import logo from '../assets/images/logos/Recurso 18.png'; // 👈 Importa tu logo aquí
 
 
 const Header = () => {
@@ -31,27 +31,37 @@ const Header = () => {
 
     return (
       <Navbar bg='dark' variant='dark' expand='lg' sticky='top'>
-        <Container>
-        <Navbar.Brand onClick={handleLogoClick} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
-          {/* <img
-            src={logo}
-            alt="Logo MLS International Logistics"
-            height="40"
-            className="me-2"
-          /> */}
-          <span className='text-primary fw-bold'>MLS&nbsp;</span>
-          <span className='text-light'>COMERCIO INTERNACIONAL</span>
-        </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-            <Nav className='ms-auto'>              
+        {/* Container fluid para evitar padding excesivo en móvil */}
+        <Container fluid className="px-3 px-md-4">
+          {/* Contenedor principal: flex row en desktop/tablet, column en móvil */}
+          <div className="d-flex align-items-center justify-content-between w-100">
+            {/* Logo + Texto: encerrados en un div para controlar su comportamiento */}
+            <div className="header-logo-text d-flex align-items-center">
+              {/* Logo */}
+              <img
+                src={logo}
+                alt="Logo MLS Comercio Internacional"
+                height="40"
+                className="me-2"
+              />
+              {/* Texto */}
+              <span className='text-primary fw-bold me-2'>MLS&nbsp;</span>
+              <span className='text-light'>COMERCIO INTERNACIONAL</span>
+            </div>
+  
+            {/* Botón hamburguesa: siempre a la derecha */}
+            <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-auto" />
+          </div>
+  
+          {/* Menú colapsable */}
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end mt-3 mt-lg-0">
+            <Nav className='ms-auto'>
               <Nav.Link onClick={() => handleNavLinkClick('#services')}>
                 Servicios
               </Nav.Link>
               <Nav.Link onClick={() => handleNavLinkClick('#blog')}>
                 Blog
               </Nav.Link>
-
             </Nav>
           </Navbar.Collapse>
         </Container>
